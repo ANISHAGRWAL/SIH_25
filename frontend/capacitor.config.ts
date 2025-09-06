0244cb2a06b55cd40914eeb42981b1a81a28615d
rajdemodak <rajgobindadham@gmail.com>
2025-09-06 23:39:03 +0530

    frontend build changes

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,14 +3,18 @@ dotenv.config();
 
 import type { CapacitorConfig } from "@capacitor/cli";
 
+const isDev = process.env.NODE_ENV === "development";
+
 const config: CapacitorConfig = {
   appId: process.env.APP_ID || "com.example.mindmates",
   appName: process.env.APP_NAME || "mindmates",
   webDir: "out",
-  server: {
-    url: process.env.SERVER_URL || "http://localhost:3000",
-    cleartext: true,
-  },
+  server: isDev
+    ? {
+        url: process.env.SERVER_URL, // Your local dev server IP
+        cleartext: true,
+      }
+    : undefined,
 };
 
 export default config;
