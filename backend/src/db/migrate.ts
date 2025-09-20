3c88e09ab4a3c77cf572139d3de33038dbe707b5
rajdemodak <rajgobindadham@gmail.com>
2025-09-20 10:33:04 +0530

    fixed the db error

--- backend/src/db/migrate.ts
+++ backend/src/db/migrate.ts
@@ -1,6 +1,6 @@
-import { migrate } from "drizzle-orm/neon-http/migrator";
-import config from "../../drizzle.config";
-import { db } from "../db";
+import { migrate } from 'drizzle-orm/neon-serverless/migrator';
+import config from '../../drizzle.config';
+import { db } from '../db';
 
 async function main() {
   await migrate(db, { migrationsFolder: config.out! });
